066f6f43ad2b3126f5c1f7a5e822b772f83ad0f1
Fady <fadyn125.com>
2023-08-21 13:21:38 -0700

    Printf Project

--- main.h
+++ main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-#include <stdio.h>
-#include <stdlib.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...);
 
-#endif
+#endif /* MAIN.H */
