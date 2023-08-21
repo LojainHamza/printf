d79cc2a1bc9fd3a862267aa542b6d5434d7b9bb5
Fady <fadyn125.com>
2023-08-21 12:22:27 -0700

    Printf Project

---
+++ _printf.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - prints formatted output
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args, format);
+
+	int i = 0;
+	char c;
+	const char *s;
+
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					c = (char)va_arg(args, int);
+					putchar(c), i++;
+					break;
+				case 's':
+					s = va_arg(args, const char *);
+					while (*s != '\0')
+					{
+						putchar(*s), s++, i++; }
+					break;
+				case '%':
+					putchar('%'), i++;
+					break;
+				default:
+					putchar('%');
+					putchar(*format);
+					i += 2;
+					break; }
+		else
+		{
+			putchar(*format);
+			i++; }
+		format++; }
+	va_end(args);
+	return (i); }
