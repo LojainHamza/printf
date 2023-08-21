51f35fe7589651b1116e37da9cee767c99d82e58
Fady <fadyn125.com>
2023-08-21 15:11:53 -0700

    Printf Project

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int print_char(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
+
+
+#endif /* MAIN_H */
