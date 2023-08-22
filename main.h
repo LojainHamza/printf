d2b5ce70adba7add7857b4fe56bc2780d1c8ef61
Fady <fadyn125.com>
2023-08-22 04:22:00 -0700

    Printf Project

--- main.h
+++ main.h
@@ -60,5 +60,7 @@ int print_reverse(va_list types, char buffer[],
 int get_flags(const char *format, int *i);
 int print_rot13string(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
+int print_binary(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
