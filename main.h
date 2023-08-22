a4f648de4755137980720f70d3f04ac1283b1e79
Fady <fadyn125.com>
2023-08-22 03:59:53 -0700

    Printf Project

--- main.h
+++ main.h
@@ -51,6 +51,7 @@ int print_hexa_upper(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size);
-
+int print_pointer(va_list types, char buffer[],
+		int flags, int width, int precision, int size);
 
 #endif /* MAIN_H */
